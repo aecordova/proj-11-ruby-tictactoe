9e5430ff6773a9e75c3e43db280619e1b2478c6c
acordova <ae_cordova@outlook.com>
2020-01-27 17:20:46 -0600

    corected the require sentence and the names of the methods to reference correctly

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,11 +1,11 @@
-require_relative './lib/game.rb'
+require '../lib/game.rb'
 
 RSpec.describe Game do
   describe '#winning_move?' do
     it "returns true in case the move is a winner or false if it's not" do
       game = Game.new
-      results_table = ['X', 'O', 'X', 'X', 'O', 'X', 'O', 'O', 'O']
-      expect(game.winning_move?(results_table, 'O')).to eql(true)
+      game.result_table = ['X', 'O', 'X', 'X', 'O', 'X', 'O', 'O', 'O']
+      expect(game.winning_move?(game.result_table, 'O')).to eql(true)
     end
   end
 end
