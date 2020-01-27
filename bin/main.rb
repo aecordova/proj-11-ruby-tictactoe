fc79d0f726f36c1b27a6f937969d2e9e6df7a087
stickler-ci <support@stickler-ci.com>
2020-01-27 21:13:38 +0000

    Fixing style errors.

--- bin/main.rb
+++ bin/main.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # !/usr/bin/env ruby
 require_relative '../lib/player.rb'
 require_relative '../lib/game_layout.rb'
@@ -6,27 +7,27 @@ require_relative '../lib/game.rb'
 module UserInterface
   include GameLayout
   def player_info(player1, player2)
-    proceed_or_quit = ""
+    proceed_or_quit = ''
     print "\n\tEnter Player 1 username? "
-    player1.username = gets.chomp()
+    player1.username = gets.chomp
     print "\n\n\tEnter Player 2 username? "
-    player2.username = gets.chomp()
+    player2.username = gets.chomp
     puts fig('div')
     begin
       print "\n\tPlayer1 please choose [X/O] "
-      player1.x_or_o = gets.chomp().upcase
-      if ["X", "O"].include? player1.x_or_o
-        if player1.x_or_o == "X" then
-          player2.x_or_o = "O"
+      player1.x_or_o = gets.chomp.upcase
+      if %w[X O].include? player1.x_or_o
+        if player1.x_or_o == 'X'
+          player2.x_or_o = 'O'
         else
           player1.username, player2.username = player2.username, player1.username
-          player1.x_or_o = "X"
-          player2.x_or_o = "O"
+          player1.x_or_o = 'X'
+          player2.x_or_o = 'O'
         end
-        return 
+        return
       end
       raise "\n\t\tplease choose only 'X' or 'O'..."
-    rescue StandardError=>e
+    rescue StandardError => e
       puts e
       sleep 1
       print "\nTrying one more time?\t"
@@ -36,14 +37,14 @@ module UserInterface
         sleep 2
         puts "press 'q' to quit "
         proceed_or_quit = gets.chomp
-        if ["", "Q"].include? proceed_or_quit.upcase
-          if proceed_or_quit.upcase == "Q"
+        if ['', 'Q'].include? proceed_or_quit.upcase
+          if proceed_or_quit.upcase == 'Q'
             print "\nGame is quiting "
-            print "in 3..."
+            print 'in 3...'
             sleep 1
-            print "2..."
+            print '2...'
             sleep 1
-            print "1..."
+            print '1...'
             sleep 1
             exit
           else
@@ -51,7 +52,7 @@ module UserInterface
           end
         end
         raise "\t\t\t<== Wrong input!!!"
-      rescue StandardError=>e
+      rescue StandardError => e
         puts e
         retry
       end
@@ -72,7 +73,9 @@ module UserInterface
         print "\t#{players.values[player_switch]} Choose your field ?"
         game.field = gets.chomp
         raise "\t\tWrong entry!!!\n" unless (1..9).include? game.field.to_i
-        raise "\tUsed Field!!!\n" unless game.result_table[game.field.to_i - 1] == ''
+        unless game.result_table[game.field.to_i - 1] == ''
+          raise "\tUsed Field!!!\n"
+        end
 
         game.result_table[game.field.to_i - 1] = players.keys[player_switch]
         clear_scr
@@ -94,12 +97,12 @@ module UserInterface
       player_switch.zero? ? player_switch += 1 : player_switch -= 1
       game.turns_counter += 1
     end
-  end  
+  end
 end
 
 include UserInterface
-player1 = Player.new("", "")
-player2 = Player.new("", "")
+player1 = Player.new('', '')
+player2 = Player.new('', '')
 game = Game.new
 
 puts fig('title')
