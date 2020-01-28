dc28b954ef9d04d4081f904c3bbd1ba1f7df5643
stickler-ci <support@stickler-ci.com>
2020-01-28 15:23:42 +0000

    Fixing style errors.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require '../lib/game.rb'
 
 RSpec.describe Game do
   describe '#winning_move?' do
     it "returns true in case the move is a winner or false if it's not" do
       game = Game.new
-      game.result_table = ['X', 'O', 'X', 'X', 'O', 'X', 'O', 'O', 'O']
+      game.result_table = %w[X O X X O X O O O]
       expect(game.winning_move?(game.result_table, 'O')).to eql(true)
     end
   end
